bf65b1beb7aeaccbcece3fef79a7ff1ae58fe3d6
203434LAPR <203434@ids.upchiapas.edu.mx>
2021-02-08 14:25:01 -0600

    Primera version

---
+++ refresco.java
@@ -0,0 +1,47 @@
+public class Refresco {
+
+	String marca;
+	String tamaño;
+	double precio;
+	boolean descuento;
+
+	public Refresco(String marca, String tamaño, double precio, boolean descuento) {
+		this.marca = marca;
+		this.tamaño = tamaño;
+		this.precio = precio;
+		this.descuento = descuento;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String gettamaño() {
+		return tamaño;
+	}
+
+	public void settamaño(String tamaño) {
+		this.tamaño = tamaño;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public boolean isDescuento() {
+		return descuento;
+	}
+
+	public void setDescuento(boolean descuento) {
+		this.descuento = descuento;
+	}
+
+}
